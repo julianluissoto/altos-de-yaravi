8f4b312cd69c8ebd2081e39cbb72136f55664fa4
julianluissoto <conjulianlasoto@gmail.com>
2025-10-16 18:01:59 -0300

    agrego animacion

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link';
 import { Mountain } from 'lucide-react';
 import { Button } from '../ui/button';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
-        <Link href="/" className="flex items-center gap-2 font-headline text-xl font-bold mr-6">
-          <Mountain className="h-6 w-6" />
+         <Link href="/" className="flex items-center gap-2 font-headline text-xl font-bold mr-6">
+          <Image
+            src="https://res.cloudinary.com/julian-soto/image/upload/v1760542030/altos%20de%20yaravi/Sin_t%C3%ADtulo-1_n6e8ko.png"
+            alt="Altos de Yaraví Logo"
+            width={32}
+            height={32}
+            className="h-8 w-8"
+          />
           Altos de Yaraví
         </Link>
         <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
