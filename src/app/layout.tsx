f2d19ef76398aa8197e3f75478924df26a2e6644
julianluissoto <conjulianlasoto@gmail.com>
2025-12-04 12:48:48 -0300

    modifico title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Altos de Yaraví | Cabañas y Refugios en Embalse, Calamuchita',
-  description: 'Descubre nuestros refugios y cabañas en Embalse de Calamuchita, Córdoba. Un lugar ideal para el descanso, la naturaleza y la aventura. ¡Reserva tu estancia!',
+  title: 'Altos de Yaraví | Cabañas en Embalse, Calamuchita',
+  description: 'Descubre nuestras cabañas en Embalse de Calamuchita, Córdoba. Un lugar ideal para el descanso, la naturaleza y la aventura. ¡Reserva tu estancia!',
 };
 
 export default function RootLayout({
@@ -23,24 +23,26 @@ export default function RootLayout({
         />
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "LodgingBusiness",
-            "name": "Altos de Yaraví | alojamiento en Embalse",
-            "address": {
-              "@type": "PostalAddress",
-              "streetAddress": "El Yaraví 72",
-              "addressLocality": "Embalse",
-              "addressRegion": "Córdoba",
-              "postalCode": "5856",
-              "addressCountry": "AR"
-            },
-            "telephone": "",
-            "priceRange": "$$",
-            "image": "https://res.cloudinary.com/julian-soto/image/upload/v1760468586/altos%20de%20yaravi/20251003_131956_cpxqtp.jpg",
-            "description": "Departamentos en Embalse de Calamuchita, Córdoba. Ideal para el descanso, la naturaleza y la aventura.",
-            "url": "https://altosdeyaravi.com.ar" // Reemplazar con tu URL final
-          }) }}
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "LodgingBusiness",
+              "name": "Altos de Yaraví | alojamiento en Embalse",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "El Yaraví 72",
+                "addressLocality": "Embalse",
+                "addressRegion": "Córdoba",
+                "postalCode": "5856",
+                "addressCountry": "AR"
+              },
+              "telephone": "",
+              "priceRange": "$$",
+              "image": "https://res.cloudinary.com/julian-soto/image/upload/v1760468586/altos%20de%20yaravi/20251003_131956_cpxqtp.jpg",
+              "description": "Departamentos en Embalse de Calamuchita, Córdoba. Ideal para el descanso, la naturaleza y la aventura.",
+              "url": "https://altosdeyaravi.com.ar" // Reemplazar con tu URL final
+            })
+          }}
         />
       </head>
       <body className="font-body antialiased">
