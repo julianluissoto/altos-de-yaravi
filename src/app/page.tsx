0cc3b4bf8911b10ea3954c537b7254b6848f1f4e
julianluissoto <julianlasoto@gmail.com>
2025-12-18 17:48:23 -0300

    centro pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,52 +2,58 @@ import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 import Hero from '@/components/sections/hero';
 import Accommodations from '@/components/sections/accommodations';
-import Gallery from '@/components/sections/gallery';
-import Contact from '@/components/sections/contact';
+import Gallery from '@/components/sections/gallery'; import Contact from '@/components/sections/contact';
 import PlacesToVisit from '@/components/sections/places-to-visit';
 import LocationMap from '@/components/sections/location-map';
-import Reviews from '@/components/sections/reviews';
-import { AnimatedSection } from '@/components/shared/animated-section';
-import { ScrollToTopButton } from '@/components/shared/ScrollToTopButton';
-import WhereToEat from '@/components/sections/whereToEat';
+import Reviews from '@/components/sections/reviews'; import { AnimatedSection } from '@/components/shared/animated-section';
+import { ScrollToTopButton } from '@/components/shared/ScrollToTopButton'; import WhereToEat from '@/components/sections/whereToEat';
 import TouristCircuits from '@/components/sections/TouristicsCircuits';
-
-
-
 
 export default function Home() {
   return (
-    <div className="flex min-h-dvh flex-col bg-background">
-      <Header />
-      <main>
-        <Hero />
-        <AnimatedSection>
-          <Accommodations />
-        </AnimatedSection>
-        <AnimatedSection>
-          <Gallery />
-        </AnimatedSection>
-        <AnimatedSection>
-          <PlacesToVisit />
-        </AnimatedSection>
-        <AnimatedSection>
-          <TouristCircuits />
-        </AnimatedSection>
-        <AnimatedSection>
-          <WhereToEat />
-        </AnimatedSection>
-        <AnimatedSection>
-          <LocationMap />
-        </AnimatedSection>
-        <AnimatedSection>
-          <Reviews />
-        </AnimatedSection>
-        <AnimatedSection>
-          <Contact />
-        </AnimatedSection>
-      </main>
-      <Footer />
-      <ScrollToTopButton />
+    <div className="min-h-dvh bg-background">
+      <div className="flex flex-col max-w-screen-xl mx-auto">
+        <Header />
+
+        <main>
+          <Hero />
+
+          <AnimatedSection>
+            <Accommodations />
+          </AnimatedSection>
+
+          <AnimatedSection>
+            <Gallery />
+          </AnimatedSection>
+
+          <AnimatedSection>
+            <PlacesToVisit />
+          </AnimatedSection>
+
+          <AnimatedSection>
+            <TouristCircuits />
+          </AnimatedSection>
+
+          <AnimatedSection>
+            <WhereToEat />
+          </AnimatedSection>
+
+          <AnimatedSection>
+            <LocationMap />
+          </AnimatedSection>
+
+          <AnimatedSection>
+            <Reviews />
+          </AnimatedSection>
+
+          <AnimatedSection>
+            <Contact />
+          </AnimatedSection>
+        </main>
+
+        <Footer />
+        <ScrollToTopButton />
+      </div>
     </div>
   );
 }
